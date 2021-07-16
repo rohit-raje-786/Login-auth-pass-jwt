7dc46f7d954fdf5dc321718fe0a50acb07f776c6
rohit-raje-786 <rp589006@gmail.com>
2021-07-16 19:09:30 +0530

    login-auth

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://ceedmy:ceedmy123@cluster1.xrtk7.mongodb.net/newdb",
+  secretOrKey: "rohit123",
+};
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
+const admin = require("./routes/api/admin");
+const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
+
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch((err) => console.log(err));
+
+app.use(passport.initialize());
+
+require("./config/passport")(passport);
+
+app.use("/api/users", users);
+app.use("/api/admin", admin);
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
